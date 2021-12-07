38d6f1d9a0662a4e6d10d173701be1b383647846
walissonsou <you@example.com>
2021-12-06 23:02:36 -0300

    commit

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -3,41 +3,31 @@ import { FaGoogle } from 'react-icons/fa';
 import styles from './styles.module.scss';
 import { signIn, signOut, useSession } from 'next-auth/react'
 
-
 import { FiX } from 'react-icons/fi'
 
 export function SignButton() {
-   
-    const { data: session } = useSession()
 
+    const { data: session } = useSession()
     return session ? (
         <button
             type="button"
-            className={styles.singInButton} 
-           >
-
-
+            className={styles.singInButton}
+        >
             <FaGoogle color="#32f800" />
             Walisson Souza
             <FiX />
-
         </button>
     ) : (
         <button
             type="button"
             className={styles.singInButton}
             onClick={() => signIn()}>
-                 
+
             <FaGoogle color="#eba417" />
-            Acessar com o Google 
-            <FiX  
-             onClick={() => signOut()}
-             />
-            </button>
-
+            Acessar com o Google
+            <FiX
+                onClick={() => signOut()}
+            />
+        </button>
     );
-
-
-
-
 }
